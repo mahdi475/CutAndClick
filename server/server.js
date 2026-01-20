21a313316413710f5d1ab9d8fc1dbd469a03af9a
Mahdi Mosavi <mahdimousavi8909@gmail.com>
2026-01-20 09:37:39 +0100

    Första versionen: Fulltack struktur klar

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {res.json({ message: 'Backend fungerar!'})})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+})
