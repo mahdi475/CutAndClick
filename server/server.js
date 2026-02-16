25dfab2f15e907a04c60b2693acfd995290a357e
Mahdi Mosavi <mahdimousavi8909@gmail.com>
2026-02-16 14:09:36 +0100

    feat: Initialize Express server with basic routing, CORS, and server startup.

--- server/server.js
+++ server/server.js
@@ -1,13 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.get('/', (req, res) => {res.json({ message: 'Backend fungerar!'})})
+const app = require('./app');
 
 const PORT = 3000;
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
-})
+});
